74c4416c4616a562a3b56b90460c076f3c225f4d
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-01-27 20:34:12 +0000

    in lab not done

--- Vim-Workshop/vim-practice.c
+++ Vim-Workshop/vim-practice.c
@@ -1,21 +1,19 @@
 /*
- * Put your name and motto here
+* Ary Wilson rewatch lab
  */
 include <stdio.h>
 
 int main() {
-printf("Hello vim")
-int a = 0
-scanf("Enter a number: ");
-if (a < 0) {
-  print("a less than zero");
-} else if (a > 0) {
-  print("a greater than zero");
-} else {
-  print(you win!)
-}
-  
-}
+	printf("Hello vim");
+	int a = 0;
+	scanf("Enter a number: ");
+	if (a < 0) {
+  		printf("a less than zero");
+	} else if (a > 0) {
+  		print(f"a greater than zero");
+	} else {
+  		printf("you win!");
+	}  
 }
 
 
