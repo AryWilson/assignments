7333a117885b4100a4a1c0ed6d960125b5d70f40
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-01-28 21:09:02 +0000

    snackbar update

--- A01/snackbar.c
+++ A01/snackbar.c
@@ -9,12 +9,13 @@ struct snack {
 	char name[64];
 };
 
-void makeSnack(){
+int main() {
 	struct snack obj, sp, fff, fp;
+	struct snack theSnacks[4];
 	strcpy(obj.name,"onion-bannana juice");
-	strcpy(sp.name,"sea prunes");
+	strcpy(sp.name,"sea prunes         ");
 	strcpy(fff.name,"flaming fire flakes");
-	strcpy(fp.name, "fruit pies");
+	strcpy(fp.name,"fruit pies         ");
 	obj.cost = 2.90;
 	sp.cost = 8.50;
 	fff.cost = 4.50;
@@ -23,17 +24,32 @@ void makeSnack(){
 	sp.quantity = 8;
 	fff.quantity = 10;
 	fp.quantity = 0;
+	theSnacks[0] = obj;
+	theSnacks[1] = sp;
+	theSnacks[2] = fff;
+	theSnacks[3] = fp;
 	//printf("name: %s, cost: %.2f",obj.name,obj.cost);
-}
-
-int main(){
 	float money;
-	int quant;
-	makeSnack();
+	int snackID;
 	printf("Welcome to Momo's Snack Bar\n");
 	printf("How much money do you have? ");
 	scanf("%f \n",&money);
+	printf("%f \n",money);
+	for(int i=0;i<4;i++){
+		printf("%i)\t",i);
+		printf("%s \t",theSnacks[i].name);
+		printf("cost: %.2f \t",theSnacks[i].cost);
+		printf("quantity: %i \n",theSnacks[i].quantity);
+	}	
 	printf("What snack would you like to buy? [0,1,2,3] ");
-	scanf("%d \n",&quant);
+	scanf("%d \n",&snackID);
+	printf("%d \n",snackID);
+	if(money<theSnacks[snackID].cost){
+		printf("You don't have enough money! \n");
+	} else if(theSnacks[snackID].quantity<=0){
+		printf("Sorry, we are out of %s \n",theSnacks[snackID].name);
+	} else {
+		printf("You have bought %s \n",theSnacks[snackID].name);
+	}
 	return 0;
 }
