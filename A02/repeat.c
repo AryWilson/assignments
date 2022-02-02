30cd6e6af8f21da0f64303f12d0b3d84af8f691d
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-02 15:20:48 +0000

    repeat done

--- A02/repeat.c
+++ A02/repeat.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-  return 0;
+        char word[32];
+        int count;
+        printf("enter a word: ");
+        scanf("%s",word);
+        printf("\n"); 
+        printf("enter a count: ");
+        scanf("%i",&count);
+        printf("\n");
+        char *toPrint;
+        toPrint = malloc(sizeof(int)*32*count+1);
+        if(toPrint ==NULL){
+                printf("Cannot allocate new string. Exiting...\n");
+                exit(1);
+        }
+        for(int i =0;i<count;i++){
+                strcat(toPrint,word);
+        } 
+        printf("your word is: %s\n",toPrint);
+        free(toPrint);
+        toPrint = NULL;
+        return 0;
 }
