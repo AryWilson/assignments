0e47348ec89c3a681541af90e9a5c2f8952aa7a7
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-03 20:24:07 +0000

    update for wordguess

--- A02/wordguess.c
+++ A02/wordguess.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main() {
+  FILE *infile;
+  infile = fopen("smallWords.txt","r"); 
+  if (infile == NULL) {
+    printf("Error: unable to open file %s\n", "smallWords.txt");
+    exit(1);
+  }
+  srand(time(0));
+  char line[32];
+  int num;
+  fgets(line,32,infile);
+  num = atoi(line);
+  int rNum = (rand()%num) + 1;
+  for(int i=0;i<rNum;i++){
+    fgets(line,32,infile);
+  } 
+  printf("Welcome to Word Guess!\n");
+ 
+  int turn = 1; 
+  printf("Turn; %i\n",turn);
+  pr
   return 0;
 }
