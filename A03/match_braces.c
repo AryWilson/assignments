ff33cb857d43f672c66b15d6b85a94e50394d81b
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-09 21:49:16 +0000

    done with sorted

--- A03/match_braces.c
+++ A03/match_braces.c
@@ -22,6 +22,7 @@ struct node {
 // Param top: the top node of the stack (NULL if empty)
 // Returns the new top of the stack
 struct node* push(char sym, int line, int col, struct node* top) {
+  
   return NULL;
 }
 
@@ -40,8 +41,27 @@ void clear(struct node* top) {
 // Print all nodes in the given stack (from top to bottom)
 // Param top: the top node of the stack (NULL if empty)
 void print(struct node* top) {
+  if(top == NULL){ return;}
+  struct node* temp = top;
+  while(temp->next != NULL){
+    printf("(%d %d ), %c\n",temp->linenum,temp->colnum,temp->sym);
+    temp = temp->next;
+  }
+
 }
 
 int main(int argc, char* argv[]) {
+  if(argc != 2){
+    printf("Program expects 2 command line arguments. Exiting... \n");
+    exit(1);
+  }
+  FILE *infile;
+  infile = fopen(argv[1],"r"); 
+  if (infile == NULL) {
+    printf("Error: unable to open file %s\n", argv[1]);
+    exit(1);
+  }
+  
+
   return 0;
 }
--- A03/sorted_snackbar.c
+++ A03/sorted_snackbar.c
@@ -24,17 +24,93 @@ struct snack {
 // Returns the first item in the list
 struct snack* insert_sorted(struct snack* snacks, 
   const char* name, int quantity, float cost) {
+//construct n
+  struct snack* n = malloc(sizeof(struct snack));//error
+  if (n == NULL) {
+    printf("ERROR: Out of space!\n");
+    exit(1);
+  }
+  strcpy(n->name,name);
+  n->cost = cost;
+  n->quantity = quantity;
 
-  // todo
-  return NULL;
+//if list is empty
+  if(snacks==NULL){
+    n->next = NULL;
+    return n;
+  }
+
+//if n is firt in the list
+  if(strcmp(n->name,snacks->name)<=0){
+    n->next = snacks;
+    return n;
+  }
+  
+//if n is in the middle of the list
+  struct snack* temp=snacks;
+  while(temp->next != NULL){
+    struct snack* tempNext = temp->next;
+    if(strcmp((n->name),(tempNext->name))<=0){
+      temp->next = n;
+      n->next = tempNext;
+      return snacks;
+    } 
+    temp = temp->next;
+  }
+  temp->next = n;
+  return snacks;
 }
 
 // Delete (e.g. free) all nodes in the given list of snacks
 // Param snacks: the first node in the list (NULL if empty)
 void clear(struct snack* snacks) {
+  struct snack* tempHead = snacks;
+  while(snacks->next != NULL){
+    tempHead = snacks->next;
+    free(snacks);
+    snacks = NULL;
+    snacks = tempHead; 
+  }
+  free(tempHead);
+  tempHead = NULL;
+
 }
 
-int main() {
+int main() { 
+  int snackCount; 
+  printf("enter a number of snacks: ");
+  scanf("%i",&snackCount);
+  printf("\n");
+ 
+  struct snack* head = NULL;
+  for (int i = 0;i<snackCount;i++){
+    char name[32];
+    float cost;
+    int quantity;
+  
+    printf("enter a name: ");
+    scanf("%s",name);
+    printf("\n");
+    
+    printf("enter a cost: ");
+    scanf("%f",&cost);
+    printf("\n");
+
+    printf("enter a quantity: ");
+    scanf("%i",&quantity);
+    printf("\n");
+  
+    head = insert_sorted(head,name,quantity,cost);//error
+  } 
+
+  printf("Welcome to the Sorted Snackbar!\n");
+  struct snack* toPrint = head;
+  for(int i =0;i<snackCount;i++){
+    printf("%d) %s \t cost: $%.2f \t quantity: %d\n",i,toPrint->name,toPrint->cost,toPrint->quantity); 
+    toPrint = toPrint->next; 
+  }
+  clear(head);
+  head = NULL;
   return 0;
 }
 
