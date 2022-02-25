020b7289fe28269e1f13a412abdfc0de794a96bf
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-25 10:09:52 -0500

    update 1 and 2

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int ipow(int base, int exp){
+  int rtrn = 1;
+  for(int i = 0; i<exp;i++){
+    rtrn = rtrn*base;
+  }
+  //printf("base= %i, exp = %i, return = %i\n",base,exp,rtrn);
+  return rtrn;
+}
 
 int main() {
+  char bits[9];
+  printf("Enter 8 bits: ");
+  scanf(" %s",bits);
+  bits[8] = '\0';
+  printf("\n");
+  int sum = 0;
+  for(int i=0;i<8;i++){
+    int bin = bits[i] - '0';
+    int mult = ipow(2,7-i);
+    int toAdd = bin*mult;
+
+    sum = (sum + toAdd);
+  }
+  printf("Your character is: %c (%i)\n",sum,sum);
   return 0;
 }
--- A05/word.c
+++ A05/word.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main() {
+
+  char word[8];
+  printf("enter 4 characters: ");
+  scanf(" %s",word);
+  printf("\n");
+  //unsigned int base = 0x000000FF;
+  unsigned int lets[4];
+  unsigned int fin=0;
+  for (int j =0; j < 4; j++){
+    int i = 3-j;
+    lets[j] = word[i] << j*8;
+    printf("Debug: %c = %08X\n",word[i],lets[j]);
+    fin = fin|lets[j];
+  }
+  printf("Your number is: %i (0x%08x)\n",fin,fin);
+  
   return 0;
 }
