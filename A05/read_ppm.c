95f4f14e58551f48ec03d49c3b22548fc175b8bd
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-25 21:47:16 -0500

    try again monaaa

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -71,12 +71,16 @@ void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
   strcat(newname,"-glitch.ppm");
   outfile = fopen (newname, "wb");
   printf("Writing file %s\n",newname);
-  char head[64] = "P3\n";
+  /*char head[64] = "P3\n";
   sprintf(head, "%d %d\n255\n", w,h);  
-  fwrite(head,64,1,outfile);
+  fwrite(head,64,1,outfile);*/
+  
   for (int i = 0; i < h; i++) {
     for (int j = 0; j < w; j++) {
-      fwrite(&pxs[i*w + j],sizeof(struct ppm_pixel),1,outfile);
+      pxs[i*w+j].red = pxs[i*w+j].red << (rand() % 2);
+      pxs[i*w+j].green = pxs[i*w+j].green << (rand() % 2);
+      pxs[i*w+j].blue = pxs[i*w+j].blue << (rand() % 2);
+      //fwrite(&pxs[i*w + j],sizeof(struct ppm_pixel),1,outfile);
     }
   } 
 
