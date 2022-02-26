7a0a66734d5d3b73866838426de71149ed53975a
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-25 22:26:13 -0500

    try agin mona

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -89,7 +89,7 @@ void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
     }
   }*/
   char head[64];
-  sprintf(head, "P2\n%i %i\n255\n", w,h);  
+  sprintf(head, "P3\n%i %i\n255\n", w,h);  
   fwrite(head,64,1,outfile);
   
   for (int i = 0; i < h; i++) {
