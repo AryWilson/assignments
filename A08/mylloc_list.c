5c2c340e71f09bd722a691a158f0ad558cc4e61e
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-04-01 21:55:45 -0400

    assignment 8 complete

--- A08/mylloc_list.c
+++ A08/mylloc_list.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 
 struct chunk {
   int size;
+  int in_use;
   struct chunk *next;
 };
 struct chunk *flist = NULL;
@@ -12,20 +14,95 @@ void *malloc (size_t size) {
   if (size == 0){
     return NULL;
   }
-
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+  
+  while(next!=NULL){
+    if(next->size >= size){
+      if(prev != NULL){
+        prev->next = next->next;  
+      }else {flist = next->next;}
+      next->in_use = size;
+      return (void*)(next+1);
+    }else {
+      prev = next;
+      next = next->next;
+    }
+  }
+  
   void *memory = sbrk(size + sizeof(struct chunk));
   if (memory == (void *) -1) {
     return NULL;
   } else {
-    struct chunk* cnk = (struct chunk*) memory;
-    return (void*) (cnk + 1);
+    struct chunk* new = (struct chunk*) memory;
+    new->size = size;
+    new->in_use = size;
+    return (void*) (new + 1);
   }
 }
 
 void free(void *memory) {
+  if (memory !=NULL){
+    struct chunk *new = (struct chunk*)((struct chunk*)memory-1);
+    new->next = flist;
+    flist = new; 
+  }
   return;
 }
 
 void fragstats(void* buffers[], int len) {
+
+  int tInUnu = 0;
+  int tExUnu = 0;
+  int uCount = 0;
+  int fCount = 0;
+  int inS=0;
+  int inL=0;
+  int exS=0;
+  int exL=0;
+  
+  for(int i = 0;i<len;i++){
+    if(buffers[i]!=NULL){ 
+      
+      struct chunk *buf = (struct chunk*)((struct chunk*) buffers[i]-1);
+      uCount+=1;
+      int s = buf->size;
+      int inu = buf->in_use;
+      int unu = s-inu;
+      tInUnu += unu;
+      if(inS==0){inS=unu;}
+      if(unu>inL){
+        inL = unu;
+      }else if (unu<inS){
+        inS = unu;
+      }
+    }
+  }
+  struct chunk *cnk = flist;
+  while(cnk->next!=NULL){
+    int s = cnk->size;
+    int inu = cnk->in_use;
+    int unu = s-inu;
+    fCount+=1;
+    tExUnu+=s;
+    if(exS==0){exS=unu;}
+    
+    if(unu>exL){
+      exL = unu;
+    }else if ((unu<exS)&&(unu>0)){
+      exS = unu;
+    }
+    cnk=cnk->next;
+  }
+  
+
+  printf("Total blocks: %d Free: %d Used: %d\n",uCount+fCount,fCount,uCount);
+  printf("(Internal unused) total: %d average: %.1f smallest: %d largest: %d\n",
+                                   tInUnu,(float)tInUnu/uCount,inS,        inL);
+  printf("(External unused) total: %d average: %.1f smallest: %d largest: %d\n",  
+                                   tExUnu,(float)tExUnu/fCount,exS,        exL);
+  
+
+
 }
 
--- A08/reallocate.c
+++ A08/reallocate.c
@@ -5,6 +5,25 @@
 #include "rand.h"
 
 int main (int argc, char* argv[]) {
+  void *init = sbrk(0);
+  void* current;
+  void* end; 
+  for (int i = 1; i <= 10; i++){
+    int *memory = malloc(100);
+    if(memory == NULL){
+      printf("malloc failed\n");
+      exit(1);
+    }
+    
+  current = sbrk(0);
+  int allocated = (int) (( current - init)); // /1.024
+  printf("Round: %d \nCurrent top of heap = %p\nIncrease: %d Bytes\n",i,current,allocated);
+  *memory = 123;
+  free(memory);  
+  memory = NULL;
+  end = sbrk(0); 
+  } 
 
+  printf("After all frees: %p\n",end);
   return 0 ;
 }
